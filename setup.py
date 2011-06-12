8646004ec891ffc1b8d3b55ff1cefd3833f2a498
Ian Davis <nospam@iandavis.com>
2011-06-12 21:39:51 +0100

    Added syntax highlighting with pygments

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup
 setup(
     name = 'pytassium',
-    version = '0.1.8',
+    version = '0.1.10',
     description = 'A Python library for working with Kasabi.com APIs',
     author='Ian Davis',
     author_email='nospam@iandavis.com',
     url='https://github.com/iand/pytassium',
     classifiers=['Programming Language :: Python','License :: Public Domain', 'Operating System :: OS Independent', 'Development Status :: 4 - Beta', 'Intended Audience :: Developers', 'Topic :: Software Development :: Libraries :: Python Modules', 'Topic :: Database'],
     packages =['pytassium'],
-    install_requires=['rdfchangesets'],
+    install_requires=['rdfchangesets', 'TurtleLexer'],
     scripts = ["scripts/pytassium"],
     
 )
