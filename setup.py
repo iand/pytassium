576557e544ef46ee599390f5295dc5a6ab25894d
Ian Davis <nospam@iandavis.com>
2011-06-13 16:17:52 +0100

    pytassium command line util now recognizes the KASABI_API_KEY environment variable

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 setup(
     name = 'pytassium',
-    version = '0.1.11',
+    version = '0.2.0',
     description = 'A Python library for working with Kasabi.com APIs',
     author='Ian Davis',
     author_email='nospam@iandavis.com',
