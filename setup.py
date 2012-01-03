55eb5556b07f119cdb7ffbde48c24e31bfb97f97
Ian Davis <nospam@iandavis.com>
2012-01-03 11:12:44 +0000

    Improved status messages for chunked upload of data

--- setup.py
+++ setup.py
@@ -1,7 +1,13 @@
 from setuptools import setup
+import os, re
+name = 'pytassium'
+version_number_re = "\s*__version__\s*=\s*((\"([^\"]|\\\\\")*\"|'([^']|\\\\')*'))"
+version_file = os.path.join(os.path.dirname(__file__), name, '__init__.py')
+version_number = re.search(version_number_re, open(version_file).read()).groups()[0][1:-1]
+
 setup(
-    name = 'pytassium',
-    version = '0.2.4',
+    name = name,
+    version = version_number,
     description = 'A Python library for working with Kasabi.com APIs',
     author='Ian Davis',
     author_email='nospam@iandavis.com',
